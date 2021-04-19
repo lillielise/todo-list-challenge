dfde6c130f6aa526870013156c50062b59c8021a
Lillian Gales <lilliangales@ymail.com>
2021-04-19 12:06:30 -0700

    added typescript to project

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,31 @@
+import React from "react";
 import "../styles/TodoList.css";
 import { FaTrash, FaExclamationCircle } from "react-icons/fa";
+
+type Props = {
+  todoListItems: Array<object>;
+  handleDeleteItem: Function;
+  handlePrioritizeItem: Function;
+};
+
+// TODO: come back and fix the any type
+// interface Item {
+//   value: { id: Number; name: String; priority: Number };
+// }
+
+// type Items = {
+//   id: Number;
+//   name: String;
+//   priority: Number;
+// };
 
 const TodoList = ({
   todoListItems,
   handleDeleteItem,
   handlePrioritizeItem,
-}) => {
+}: Props) => {
   const renderTodoListItems = () => {
-    return todoListItems.map((item) => (
+    return todoListItems.map((item: any) => (
       <li className="list-item" key={`todo-${item.id}`}>
         <p>{item.name}</p>
         <div className="list-item-icons">
